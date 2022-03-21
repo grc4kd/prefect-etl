a6e6b7bcdc5a4e19e148220a1f229e54d5b9bd15
Griffin Crane <grc4kd@users.noreply.github.com>
2022-03-20 22:17:17 -0500

    made func a litte more interesting

--- flows/tutorial_03.py
+++ flows/tutorial_03.py
@@ -5,21 +5,35 @@ from prefect import task, Flow
 def extract():
     logger = prefect.context.get("logger")
     logger.info("Extracting...")
-    all_data = [123, 234, 345, 456, 567, 234, 456, 123]
+    all_data = [123, "234", "ball", 0, 345, 456, 567, 234, 456, 123]
     return all_data
 
 @task
 def transform(all_data):
     logger = prefect.context.get("logger")
     logger.info(f"Transforming {len(all_data)} array elements.")
+
+    def subt(elem):
+        x = 0
+        try:
+            x = int(elem)
+        except Exception:
+            x = 0
+        if x != 0:
+            return 2 / x
+        if x == 0:
+            return int.from_bytes(b'\xff\xff', byteorder='big', signed=True)
     # divide by two
     # if input is zero return zero (don't divide by zero)
-    return [0 if 0 else x / 2 for x in all_data]
+    return [subt(x) for x in all_data]
 
 @task
 def load(all_data):
     logger = prefect.context.get("logger")
-    logger.info("Loading...")
+    logger.info("Loading data...")
+    # normally we would want to keep the data
+    # OUT of the prefect cloud logs
+    logger.info(str(all_data))
     # write / load data to new text file
     with open("new_data.txt", "w") as f:
         f.write(str(all_data) + "\n")
@@ -32,3 +46,4 @@ with Flow("etl-flow") as flow:
 
 # Register the flow under the "tutorial" project
 flow.register(project_name="tutorial")
+#flow.run()
