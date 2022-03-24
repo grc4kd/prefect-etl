37724720dfb4e9907e103584af5bbe3a24cebe5c
Griffin Crane <grc4kd@users.noreply.github.com>
2022-03-23 20:02:33 -0500

    using mapping concepts

--- flows/tutorial_docker_agent.py
+++ flows/tutorial_docker_agent.py
@@ -1,5 +1,5 @@
 import prefect
-from prefect import Flow, task
+from prefect import Flow, task, flatten
 from prefect.run_configs import DockerRun
 from prefect.storage import GitHub
 from prefect.executors import DaskExecutor
@@ -14,20 +14,9 @@ def extract():
 
 
 @task
-def join_data_lists(data_lists):
-    # unpack lists and make one long list
+def transform(data):
     logger = prefect.context.get("logger")
-    logger.info("Joining...")
-    all_data = []
-    for data in data_lists:
-        all_data.extend(data)
-    return all_data
-
-
-@task
-def transform(all_data):
-    logger = prefect.context.get("logger")
-    logger.info(f"Transforming {len(all_data)} array elements.")
+    logger.info(f"Transforming {len(data)} array elements.")
 
     # a sub-routine to do some quick math
     def subt(elem):
@@ -47,19 +36,19 @@ def transform(all_data):
         if x == 0:
             return int.from_bytes(b'\xff\xff', byteorder='big', signed=True)
 
-    return [subt(x) for x in all_data]
+    return [subt(x) for x in data]
 
 
 @task
-def load(all_data):
+def load(data):
     logger = prefect.context.get("logger")
     logger.info("Loading data...")
     # normally we would want to keep the data
     # OUT of the prefect cloud logs
-    logger.info(str(all_data))
+    logger.info(str(data))
     # write / load data to new text file
     with open("new_data.txt", "w") as f:
-        f.write(str(all_data) + "\n")
+        f.write(str(data) + "\n")
         f.close()
 
 
@@ -69,14 +58,14 @@ with Flow("test_docker_agent") as flow:
     flow.run_config = DockerRun(
         image="prefecthq/prefect:latest"
     )
-    # let's run a bunch of functions x10
-    data_ext = [extract() for i in range(10)]
-    # join extract outputs into one big list
-    data_join = join_data_lists(data_ext)
-    # let's make transform(de) depend on the product of extract() list comprehension
-    data_trn = [transform(data_join) for i in range(3)]
-    for dt in data_trn:
-        load(dt)
+    # let's run a bunch of functions x10 range mapping
+    data_list = [extract() for i in range(10)]
+
+    # join extract outputs into one big list by using flat-mapping
+    data_trn = transform.map(data=flatten(data_list))
+
+    # load the data using a mapping function
+    load.map(data=data_trn)
 
 # storage can point to the same module
 flow.storage = GitHub(repo="grc4kd/prefect-etl",
