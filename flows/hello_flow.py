041b4b129d8f6df59dac0f65db7c4d0e83bbf4e7
Griffin Crane <grc4kd@users.noreply.github.com>
2022-04-05 00:07:34 -0500

    update to use github storage

--- flows/hello_flow.py
+++ flows/hello_flow.py
@@ -1,5 +1,6 @@
 import prefect
 from prefect import task, Flow
+from prefect.storage import GitHub
 
 @task
 def hello_task():
@@ -9,4 +10,7 @@ def hello_task():
 with Flow("hello-flow") as flow:
     hello_task()
 
-flow.run()
+# storage can point to the same module
+flow.storage = GitHub(repo="grc4kd/prefect-etl",
+                      path="/flows/hello_flow.py",
+                      access_token_secret="github_secret_grc4kd")
